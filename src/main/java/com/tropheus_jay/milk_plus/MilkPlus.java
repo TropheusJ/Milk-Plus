b217306c8cc2a0fd7b44c61c0c8d47312811615a
Tropheus Jay <jverrellijr@icloud.com>
2021-07-06 23:45:47 -0400

    Revert "colors WIP. todo: find out why some are wrong"
    This reverts commit f69c56d1

--- src/main/java/com/tropheus_jay/milk_plus/MilkPlus.java
+++ src/main/java/com/tropheus_jay/milk_plus/MilkPlus.java
@@ -80,7 +80,6 @@ public class MilkPlus implements ModInitializer {
 	
 		resetArraySimplification();
 		
-		// the pain is unbearable
 		ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.BAT_MILK_BUCKET; ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.BEE_MILK_BUCKET;
 		ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.BLAZE_MILK_BUCKET; ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.CAT_MILK_BUCKET;
 		ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.CAVE_SPIDER_MILK_BUCKET; ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.CHICKEN_MILK_BUCKET;
@@ -111,27 +110,6 @@ public class MilkPlus implements ModInitializer {
 		ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.ZOMBIE_MILK_BUCKET; ITEMS[simplifyRepetitiveArrays()] = NotEnoughMilk.ZOMBIFIED_PIGLIN_MILK_BUCKET;
 		
 		resetArraySimplification();
-		
-		// end me
-		COLORS[simplifyRepetitiveArrays()] = 0x524020; COLORS[simplifyRepetitiveArrays()] = 0xFFC328; COLORS[simplifyRepetitiveArrays()] = 0xF58026;
-		COLORS[simplifyRepetitiveArrays()] = 0xF6EF9B; COLORS[simplifyRepetitiveArrays()] = 0x1A291C; COLORS[simplifyRepetitiveArrays()] = 0xFFFFFF;
-		COLORS[simplifyRepetitiveArrays()] = 0x3DD13D; COLORS[simplifyRepetitiveArrays()] = 0x55B7CF; COLORS[simplifyRepetitiveArrays()] = 0x9E7857;
-		COLORS[simplifyRepetitiveArrays()] = 0x32A76E; COLORS[simplifyRepetitiveArrays()] = 0x1D011B; COLORS[simplifyRepetitiveArrays()] = 0x1D011B;
-		COLORS[simplifyRepetitiveArrays()] = 0x2C0F32; COLORS[simplifyRepetitiveArrays()] = 0xFEE04E; COLORS[simplifyRepetitiveArrays()] = 0x0EF6F6;
-		COLORS[simplifyRepetitiveArrays()] = 0xFFD2A9; COLORS[simplifyRepetitiveArrays()] = 0xC8B089; COLORS[simplifyRepetitiveArrays()] = 0x49C386;
-		COLORS[simplifyRepetitiveArrays()] = 0xB68572; COLORS[simplifyRepetitiveArrays()] = 0xCB9273; COLORS[simplifyRepetitiveArrays()] = 0xEBDACB;
-		COLORS[simplifyRepetitiveArrays()] = 0xF3EDE8; COLORS[simplifyRepetitiveArrays()] = 0x8F3825; COLORS[simplifyRepetitiveArrays()] = 0x8F3825;
-		COLORS[simplifyRepetitiveArrays()] = 0x9C6E3D; COLORS[simplifyRepetitiveArrays()] = 0xEFDA8C; COLORS[simplifyRepetitiveArrays()] = 0xC2E2B4;
-		COLORS[simplifyRepetitiveArrays()] = 0xE3FCC8; COLORS[simplifyRepetitiveArrays()] = 0x1D283C; COLORS[simplifyRepetitiveArrays()] = 0xE3AAE1;
-		COLORS[simplifyRepetitiveArrays()] = 0xA06761; COLORS[simplifyRepetitiveArrays()] = 0x270D20; COLORS[simplifyRepetitiveArrays()] = 0xD7C4B0;
-		COLORS[simplifyRepetitiveArrays()] = 0x8097B1; COLORS[simplifyRepetitiveArrays()] = 0xB99B56; COLORS[simplifyRepetitiveArrays()] = 0x2A2D32;
-		COLORS[simplifyRepetitiveArrays()] = 0xB49E8A; COLORS[simplifyRepetitiveArrays()] = 0xE2C4EB; COLORS[simplifyRepetitiveArrays()] = 0x626D76;
-		COLORS[simplifyRepetitiveArrays()] = 0xDADADA; COLORS[simplifyRepetitiveArrays()] = 0x68BE6B; COLORS[simplifyRepetitiveArrays()] = 0xBFF0F0;
-		COLORS[simplifyRepetitiveArrays()] = 0x500F0B; COLORS[simplifyRepetitiveArrays()] = 0x16171E; COLORS[simplifyRepetitiveArrays()] = 0xA3A5B4;
-		COLORS[simplifyRepetitiveArrays()] = 0xEB635B; COLORS[simplifyRepetitiveArrays()] = 0x56A356; COLORS[simplifyRepetitiveArrays()] = 0x73BEBC;
-		COLORS[simplifyRepetitiveArrays()] = 0xEBC7AB; COLORS[simplifyRepetitiveArrays()] = 0x144649; COLORS[simplifyRepetitiveArrays()] = 0x593674;
-		COLORS[simplifyRepetitiveArrays()] = 0x1B1B1B; COLORS[simplifyRepetitiveArrays()] = 0x8E7965; COLORS[simplifyRepetitiveArrays()] = 0x618B43;
-		COLORS[simplifyRepetitiveArrays()] = 0x1C4E22; COLORS[simplifyRepetitiveArrays()] = 0x716547;
 		
 		for (int index = 0; index < 56; index++) {
 			STILL_FLUIDS[index] = new GenericFluid.Still();
